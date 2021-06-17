5ece9e5e6717002fbb4afe2fcb4de2c6a385ef58
ctahk <85041256+ctahk@users.noreply.github.com>
2021-06-17 03:35:41 +0000

    completed function gradeQuiz

--- candidate-testing.js
+++ candidate-testing.js
@@ -14,13 +14,15 @@ let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5+3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
-
+let numCorrect = 0;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 
 candidateName = input.question("Candidate Name: "); 
+
 }
+
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
@@ -30,57 +32,52 @@ function askQuestion() {
     console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}\n`);
   }
   
+  candidateAnswers = candidateAnswers.toString().toUpperCase().split(',');
+  correctAnswers = correctAnswers.toString().toUpperCase().split(',');
+  
+  
+  for (let i = 0; i < correctAnswers.length; i++) {
+    if (candidateAnswers[i] === correctAnswers[i]){
+      numCorrect ++;
+    }
+  }
+  
 }
 
-
-//candidateAnswers = candidateAnswers.toString().toUpperCase().split(',');
-    
 
 
 function gradeQuiz(candidateAnswers) {
   
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  //TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly//
   
-  /*for (let i = 0; i < correctAnswers.length; i++) {
-    if (candidateAnswers[i] === correctAnswers[i]) {
-      console.log("all correct");
-    } else {
-      console.log("try again");
-    }
-  }
-  /*
-/* let array1 = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let array2 = ["sally ride", "true", "40", "trajectory", "3"];
-
-array1 = array1.toString().toUpperCase().split(',');
-array2 = array2.toString().toUpperCase().split(',');
-
-for (let i = 0; i < array1.length; i++) {
-  if (array1[i] === array2[i]) {
-  console.log("true");
+  let grade = (numCorrect / questions.length)* 100;
+  console.log(`>>> Overall Grade: ${grade}% (${numCorrect} of ${questions.length} responses correct) <<<`);
+  if (grade >= 80) {
+    console.log(`>>> Status: PASSED <<<`);
   } else {
-  console.log("false");
+    console.log(`>>> Status: FAILED <<<`);
   }
-} */
 
-  let grade;
- 
-  
   return grade;
 }
 
-gradeQuiz();
+
 
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
 
-  // console.log(`Hello and welcome, ${candidateName}!`);
+  //console.log(`Hello and welcome, ${candidateName}!`);
+  
 
   askQuestion();
+
+  
+
   gradeQuiz(this.candidateAnswers);
+
+
 }
 
 // Don't write any code below this line //
