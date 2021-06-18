5f5e2e8798cd5376fae542597e59ea06acf90919
ctahk <85041256+ctahk@users.noreply.github.com>
2021-06-18 02:05:37 +0000

    try again

--- candidate-testing.js
+++ candidate-testing.js
@@ -36,7 +36,7 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
   
   //TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly//
-  let numCorrect = 0;
+  let numCorrect = Number();
   for (let i = 0; i < correctAnswers.length; i++) {
     if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
       numCorrect ++;
